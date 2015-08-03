8cf86fc5dea3b5fc0d530b0d128a53bb782a557a
tevjef <mastermindtj94@gmail.com>
2015-08-03 16:40:35 -0400

    testing gin

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
-package rmp_server
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+var DB = make(map[string]string)
+
+func main() {
+	DB["Tevin"] = "Tevin Jeffrey"
+	DB["Shariis"] = "Shariis Jeffrey"
+	DB["Tyrece"] = "Tyrece Jeffrey"
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		if value, ok := DB[c.Query("name")]; ok {
+			c.String(200, "Congrats " + value)
+		} else {
+			c.String(200, "User not found")
+		}
+	})
+	r.Run(":8080")
+}
